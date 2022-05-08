067eb4247784aa8e8ea102cccff97636d7880b8c
Iris <iris.viana@catskillet.com>
2022-05-08 16:33:04 -0300

    feat: adicionando relacionamento entre tabelas veiculos e categorias e veiculos

---
+++ database/migrations/2022_05_08_162512_update_veiculos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('veiculos', function (Blueprint $table) {
+            $table->foreign('categorias_veiculos_id')
+                ->references('id')
+                ->on('categoria_veiculos')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
