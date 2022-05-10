e9ca5884fc68a9f382661c299e388077206dda43
Raquell Vieira <raquel.vieira@showtecnologia.com>
2022-05-10 20:48:58 -0300

    Adicionado relaçao usuario e categoria veiculos

--- app/Models/CategoriaVeiculo.php
+++ app/Models/CategoriaVeiculo.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CategoriaVeiculo extends Model
 {
     protected $fillable = ['descricao'];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
