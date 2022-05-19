46810e3025eaf9a2f1e7447bd8e21f94ac5527de
Raquell Vieira <raquel.vieira@showtecnologia.com>
2022-05-18 23:09:33 -0300

    Adicionado controler  de maquina

---
+++ app/Http/Controllers/MaquinaController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Maquina;
+use App\Models\Fabrica;
+use App\Models\Equipamento;
+
+use App\Http\Requests\StoreMaquinaRequest;
+use App\Http\Requests\UpdateMaquinaRequest;
+
+
+class MaquinaController extends Controller
+{
+
+    public function __construct()
+    {
+
+        $this->authorizeResource(Maquina:: class, 'maquina');
+    }
+
+
+    public function index()
+    {
+        $fabrica = Fabrica::where('user_id', auth()->user()->id)->first();
+        $maquinas = Maquina::where('fabrica_id', $fabrica->id)->get();
+
+        return view('maquina.index', compact('maquinas'));
+    }
+
+    public function create()
+    {
+       
+        $fabrica = Fabrica::where('user_id', auth()->user()->id)->first();
+        $equipamentos = Equipamento::where('fabricas_id', $fabrica->id)->get();
+        return view('maquina.create', compact('equipamentos', 'fabrica'));
+    }
+
+    public function store(StoreMaquinaRequest $request)
+    {
+        
+        $maquina = new Maquina($request->validated());
+        $maquina->save();
+        return redirect()->route('maquinas.create')->withStatus("Máquina salva com sucesso!");
+    }
+
+    public function edit(Maquina $maquina)
+    {
+        $fabrica = Fabrica::where('user_id', auth()->user()->id)->first();
+        $equipamentos = Equipamento::where('fabricas_id', $fabrica->id)->get();
+        return view('maquina.edit', compact('maquina', 'equipamentos', 'fabrica'));
+    }
+
+    public function update(UpdateMaquinaRequest $request, Maquina $maquina)
+    {
+
+        $maquina->marca = $request->marca;
+        $maquina->modelo = $request->modelo;
+        $maquina->placa = $request->placa;
+        $maquina->chassi = $request->chassi;
+        $maquina->ano = $request->ano;
+        $maquina->equipamento_id = $request->equipamento_id;
+        $maquina->fabrica_id = $request->fabrica_id;
+        $maquina->update();
+        $maquina->save();
+
+        return redirect()->route('maquinas.edit', $maquina)->withStatus("Máquina atualizada com sucesso!");;
+    }
+
+    public function show(Maquina $maquina)
+    {
+        $fabrica = Fabrica::where('user_id', auth()->user()->id)->first();
+        $equipamentos = Equipamento::where('fabricas_id', $fabrica->id)->get();
+        return view('maquina.show', compact('maquina', 'equipamentos', 'fabrica'));
+    }
+
+    public function deletar($identifier)
+    {
+        $this->maquinas->find($identifier)->delete();
+        return redirect()->route('maquinass');
+    }
+}
