21059410beaddbabd0a1c90ec4e7af3804bfecb4
Raquell Vieira <raquel.vieira@showtecnologia.com>
2022-05-18 23:10:43 -0300

    Adicionado arquivos de leviwire de maquina

---
+++ resources/views/livewire/deletar-maquina.blade.php
@@ -0,0 +1,46 @@
+<div>
+    @switch($tipo)
+        @case('icon')
+            <a wire:click="toggleModal" href="#" title="Deletar máquina">
+                @switch($cor)
+                    @case('white')
+                        <img src="{{asset('img/trash-white.svg')}}" class="w-6" alt="botao deletar">
+                        @break
+                    @case('primary')
+                    @default
+                        <img src="{{asset('img/trash-primary.svg')}}" class="w-6" alt="botao deletar">
+                    @break
+                @endswitch
+            </a>
+            @break
+        @default
+            <x-button wire:click.prevent="toggleModal">
+                @switch($cor)
+                    @case('white')
+                        <img src="{{asset('img/trash-white.svg')}}" class="w-6 mr-1" alt="botao deletar">
+                        @break
+                    @case('primary')
+                    @default
+                        <img src="{{asset('img/trash-primary.svg')}}" class="w-6 mr-1" alt="botao deletar">
+                        @break
+                @endswitch
+                Deletar máquina
+            </x-button>
+            @break
+    @endswitch
+
+
+
+    <x-jet-confirmation-modal wire:model="show">
+        <x-slot name="title">Confirmar ação</x-slot>
+        <x-slot name="content">Tem certeja que deseja deletar essa máquina?</x-slot>
+        <x-slot name="footer">
+            <x-button class="bg-red-600 hover:bg-red-700" wire:click.prevent="toggleModal" wire:loading.attr="disabled">
+                Cancelar
+            </x-button>
+            <x-button class="ml-2" wire:click.prevent="deletar" wire:loading.attr="disabled">
+                Deletar máquina
+            </x-button>
+        </x-slot>
+    </x-jet-confirmation-modal>
+</div>
