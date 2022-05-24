c52333e3b09aa38834b57806684abce6a94070eb
LuisFelipePereiraDaSilva <luisfelipepereira150@gmail.com>
2022-05-23 22:50:35 -0300

    Adicionado mensagens de listas vazias

--- resources/views/equipamento/index.blade.php
+++ resources/views/equipamento/index.blade.php
@@ -10,21 +10,29 @@
             </div>
         @endif
         <div class="organosoft-list">
-            @foreach ($equipamentos as $equipamento)
-                <div class="organosoft-list__item">
-                    <div class="w-5/6 flex flex-wrap">
-                        <a href="{{route('equipamentos.show', $equipamento)}}" class="flex w-full text-primary text-3xl font-bold">{{$equipamento->nome}}</a>
-                        <p class="text-sm text-gray-500">{{$equipamento->data_compra}}</p>
+            @if(count($equipamentos) > 0)
+                @foreach ($equipamentos as $equipamento)
+                    <div class="organosoft-list__item">
+                        <div class="w-5/6 flex flex-wrap">
+                            <a href="{{route('equipamentos.show', $equipamento)}}" class="flex w-full text-primary text-3xl font-bold">{{$equipamento->nome}}</a>
+                            <p class="text-sm text-gray-500">{{$equipamento->data_compra}}</p>
+                        </div>
+                        <div class="flex place-items-center">
+                            <a class="mr-2 w-6" href="{{route('equipamentos.edit', $equipamento)}}" title="Editar equipamento">
+                                <img src="{{asset('img/pencil-primary.svg')}}" alt="link editar">
+                            </a>
+                            <livewire:deletar-equipamento :equipamento="$equipamento" :tipo="'icon'">
+                        </div>
                     </div>
-                    <div class="flex place-items-center">
-                        <a class="mr-2 w-6" href="{{route('equipamentos.edit', $equipamento)}}" title="Editar equipamento">
-                            <img src="{{asset('img/pencil-primary.svg')}}" alt="link editar">
-                        </a>
-                        <livewire:deletar-equipamento :equipamento="$equipamento" :tipo="'icon'">
-                    </div>
+                    <hr class="mb-2">
+                @endforeach
+            @else
+              <div class="organosoft-list__item  justify-center">
+                <div class="organosoft-list__item__title">
+                  <p>Nenhum equipamento cadastrado</p>
                 </div>
-                <hr class="mb-2">
-            @endforeach
+              </div>
+            @endif
         </div>
     </x-slot>
     <x-slot name="side_content">
--- resources/views/veiculo/index.blade.php
+++ resources/views/veiculo/index.blade.php
@@ -10,29 +10,37 @@
             </div>
         @endif
         <div class="organosoft-list">
-            @foreach ($veiculos as $veiculo)
-                <div class="organosoft-list__item">
-                    <div class="organosoft-list__item__icon">
-                        <img src="{{asset('img/car.svg')}}" alt="link editar">
+            @if(count($veiculos) > 0)
+                @foreach ($veiculos as $veiculo)
+                    <div class="organosoft-list__item">
+                        <div class="organosoft-list__item__icon">
+                            <img src="{{asset('img/car.svg')}}" alt="link editar">
+                        </div>
+                        <div class="w-5/6 flex flex-wrap">
+                            <a href="{{route('veiculos.show', $veiculo)}}" class="flex w-full text-primary text-3xl font-bold">{{$veiculo->marca}}</a>
+                            <p class="text-sm text-gray-500">{{$veiculo->placa}}</p>
+                        </div>
+                        <div class="flex place-items-center">
+                            <a class="mr-2 w-6" href="{{route('veiculos.edit', $veiculo)}}" title="Editar veículo">
+                                <img src="{{asset('img/pencil-primary.svg')}}" alt="link editar">
+                            </a>
+                            <livewire:deletar-veiculo :veiculo="$veiculo" :tipo="'icon'">
+                        </div>
                     </div>
-                    <div class="w-5/6 flex flex-wrap">
-                        <a href="{{route('veiculos.show', $veiculo)}}" class="flex w-full text-primary text-3xl font-bold">{{$veiculo->marca}}</a>
-                        <p class="text-sm text-gray-500">{{$veiculo->placa}}</p>
-                    </div>
-                    <div class="flex place-items-center">
-                        <a class="mr-2 w-6" href="{{route('veiculos.edit', $veiculo)}}" title="Editar veículo">
-                            <img src="{{asset('img/pencil-primary.svg')}}" alt="link editar">
-                        </a>
-                        <livewire:deletar-veiculo :veiculo="$veiculo" :tipo="'icon'">
-                    </div>
+                    <hr class="mb-2">
+                @endforeach
+            @else
+              <div class="organosoft-list__item  justify-center">
+                <div class="organosoft-list__item__title">
+                  <p>Nenhum veículo cadastrado</p>
                 </div>
-                <hr class="mb-2">
-            @endforeach
+              </div>
+            @endif
         </div>
     </x-slot>
     <x-slot name="side_content">
         <x-link href="{{route('veiculos.create')}}">
-            <img src="{{asset('img/plus-white.svg')}}" alt="icon plus"> 
+            <img src="{{asset('img/plus-white.svg')}}" alt="icon plus">
             Adicionar veículos
         </x-link>
         <x-card class="h-32"></x-card>
