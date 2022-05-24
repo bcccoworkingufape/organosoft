c494bb4dc954e934d75c7faeabbf596e8cfa8bf7
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-05-24 07:43:03 -0300

    fecha o menu ao clicar fora

--- resources/views/livewire/dropdown-menu.blade.php
+++ resources/views/livewire/dropdown-menu.blade.php
@@ -1,7 +1,7 @@
 <div class="organosoft-menu">
     <button type="button" wire:click="toggleMenu" class="organosoft-menu__toggle">
         {{ $name }}
-        <div class="organosoft-menu__dropdown {{ $show ? 'show' : '' }}">
+        <div class="organosoft-menu__dropdown {{ $show ? 'show' : '' }}" @click.outside="$wire.set('show', false)">
             @foreach ($links as $link)
                 <a href="{{ $link['route'] }}" class="organosoft-menu__menu-item {{ $link['active'] ? 'active' : '' }}">
                     {{ $link['name'] }}
